1e0c5445752961a79655176c449872944650381c
taufan12345 <abaysurbakti17@gmail.com>
2025-05-14 10:26:42 +0700

    fix bug

--- src/sections/Landing/AboutLanding.vue
+++ src/sections/Landing/AboutLanding.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="relative w-full pt-20 md:pt-32 flex items-center pb-32">
-    <div class="container relative md:px-16 lg:px-20 flex w-full z-10">
-      <img
-        src=""
-        alt=""
-        class="absolute top-0 right-0 -translate-x-1/2 -translate-y-1/2 w-[600px] opacity-50 pointer-events-none"
-      />
-      <div class="flex-1 py-20">
+  <section
+    class="relative w-full pt-20 md:pt-32 flex items-center pb-32 overflow-hidden"
+  >
+    <div
+      class="container relative md:px-16 lg:px-20 flex flex-col md:flex-row w-full z-10"
+    >
+      <!-- Konten Teks -->
+      <div class="flex-1 py-20 relative z-10">
         <h1 class="text-5xl font-bold">Tentang Kami</h1>
         <p class="text-justify pt-8">
           Pijar Nusantara adalah platform yang menghubungkan relawan pendidikan
@@ -14,21 +14,26 @@
           aksi nyata dan kolaborasi berkelanjutan.
         </p>
         <div class="mt-10">
-            <div>
-
-                <Button link="/" title="Lihat Lebih Banyak" />
-            </div>
+          <Button link="/" title="Lihat Lebih Banyak" />
         </div>
       </div>
-      <div class="flex-1 px-5"></div>
+
+      <!-- Gambar Logo -->
+      <div class="flex-1 relative z-0 flex justify-end items-start pr-5">
+        <img
+          src="/images/logo.png"
+          alt="Logo Pijar Nusantara"
+          class="w-64 md:w-96 object-contain"
+        />
+      </div>
     </div>
   </section>
 </template>
 <script>
 import Button from "./../../components/button.vue";
 export default {
-    components:{
-        Button
-    }
-}
+  components: {
+    Button,
+  },
+};
 </script>
