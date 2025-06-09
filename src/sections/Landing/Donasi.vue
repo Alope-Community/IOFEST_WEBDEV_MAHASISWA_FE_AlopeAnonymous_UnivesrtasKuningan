412d28787c16bc9b59a1cfa4f338b112f21f8e42
dikrifzn <dikrifauz@gmai.com>
2025-06-09 21:39:05 +0700

    feat: Implement API at Landing Page

--- src/sections/Landing/Donasi.vue
+++ src/sections/Landing/Donasi.vue
@@ -25,17 +25,17 @@
       <!-- Scrollable Horizontal Wrapper -->
       <div class="flex gap-5 overflow-x-auto pb-2">
         <div
-          v-for="(program, index) in programs"
+          v-for="(donasi, index) in donasis"
           :key="index"
           class="min-w-[300px] border border-primary rounded overflow-hidden shadow flex flex-col bg-white"
         >
           <img
-            :src="program.image"
-            :alt="program.title"
+            :src="donasi.image_url"
+            :alt="donasi.title"
             class="w-full min-h-52 max-h-52 object-cover"
           />
           <div class="lg:p-6 p-4 flex flex-col">
-            <h3 class="text-xl font-semibold mb-4">{{ program.title }}</h3>
+            <h3 class="text-xl font-semibold mb-4">{{ donasi.title }}</h3>
             <Button link="/" title="Daftar Sekarang" />
           </div>
         </div>
@@ -45,6 +45,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import Button from "../../components/button.vue"; // ganti path sesuai struktur file kamu
 
 export default {
@@ -54,13 +55,28 @@ export default {
   },
   data() {
     return {
-      programs: [
-        { title: "Biaya Sekolah Anak Yatim", image: "https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fmaster%2F111939a5-2f80-11f0-a556-defbf884c5fd_C1D6AB5E6562169.png%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75" },
-        { title: "Wujudkan Anak Dhuafa Bisa Sekolah", image: "https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fe7529a6b-6170-496b-b742-52383a7553ab.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75" },
-        { title: "Yatim Dan Penghafal Quran", image: "https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fmaster%2F78bacea9-2f5b-11f0-a556-defbf884c5fd_936339C194169CC7.png%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75" },
-        { title: "Infaq Alquran", image: "https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fmaster%2Faaa7ced7-3021-11f0-a556-defbf884c5fd_F010CDE0F6AAB958.png%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75" },
-      ],
-    };
+      donasis: []
+    }
   },
-};
+    mounted() {
+    this.fetchDonasis();
+  },
+    methods: {
+    async fetchDonasis() {
+      try {
+        const baseUrl = import.meta.env.VITE_API_BASE_URL;
+        const response = await axios.get(`${baseUrl}/api/donasi?limit=5`);
+        
+        this.donasis = response.data.data.map(item => ({
+          ...item,
+          image_url: `${baseUrl}/storage/${item.image_url}`
+        }));
+
+        console.log(this.relawans);
+      } catch (error) {
+        console.error("Gagal mengambil data program:", error);
+      }
+    }
+  },
+  };
 </script>
--- src/sections/Landing/Relawan.vue
+++ src/sections/Landing/Relawan.vue
@@ -23,17 +23,17 @@
       <!-- Scrollable Horizontal Wrapper -->
       <div class="flex gap-5 overflow-x-auto pb-2">
         <div
-          v-for="(program, index) in programs"
+          v-for="(relawan, index) in relawans"
           :key="index"
           class="min-w-[300px] border border-primary rounded overflow-hidden shadow flex flex-col bg-white"
         >
           <img
-            :src="program.image"
-            :alt="program.title"
+            :src="relawan.image_url"
+            :alt="relawan.title"
             class="w-full min-h-52 max-h-52 object-cover"
           />
           <div class="lg:p-6 p-4 flex flex-col">
-            <h3 class="text-xl font-semibold mb-4">{{ program.title }}</h3>
+            <h3 class="text-xl font-semibold mb-4">{{ relawan.title }}</h3>
             <Button link="/" title="Daftar Sekarang" />
           </div>
         </div>
@@ -43,7 +43,8 @@
 </template>
 
 <script>
-import Button from "../../components/button.vue"; // ganti path sesuai struktur file kamu
+import axios from "axios";
+import Button from "../../components/button.vue";
 
 export default {
   name: "ProgramSection",
@@ -52,14 +53,28 @@ export default {
   },
   data() {
     return {
-      programs: [
-        { title: "Baduy Mengajar", image: "/images/relawan/1.jpg" },
-        { title: "Call Valunters Jakarta", image: "/images/relawan/2.jpg" },
-        { title: "Mengajar Indonesia Batch 14", image: "/images/relawan/3.jpg" },
-        { title: "Solidarity For Kids", image: "/images/relawan/4.jpg" },
-        { title: "Ceria Bersama", image: "/images/relawan/5.jpg" },
-      ],
-    };
+      relawans: []
+    }
+  },
+    mounted() {
+    this.fetchRelawans();
+  },
+    methods: {
+    async fetchRelawans() {
+      try {
+        const baseUrl = import.meta.env.VITE_API_BASE_URL;
+        const response = await axios.get(`${baseUrl}/api/relawan?limit=5`);
+        
+        this.relawans = response.data.data.map(item => ({
+          ...item,
+          image_url: `${baseUrl}/storage/${item.image_url}`
+        }));
+
+        console.log(this.relawans);
+      } catch (error) {
+        console.error("Gagal mengambil data program:", error);
+      }
+    }
   },
 };
 </script>
